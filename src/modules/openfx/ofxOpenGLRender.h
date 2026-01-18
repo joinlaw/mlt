55192c9a3fecdc36e7e43edd06a3a1781b4d57d1
mr.fantastic <mrfantastic@firemail.cc>
2026-01-18 16:18:48 +0300

    Moving openfx standard header files into git submodule using official repository

--- src/modules/openfx/ofxOpenGLRender.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef _ofxOpenGLRender_h_
-#define _ofxOpenGLRender_h_
-
-// Copyright OpenFX and Contributors to the OpenFX project.
-// SPDX-License-Identifier: BSD-3-Clause
-
-#endif // _ofxOpenGLRender_h_
